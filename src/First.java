82f4e35a0230f4310dfc8b5dc019aeebbae3f028
olefir vladimir <2642218@gmail.com>
2016-11-05 17:41:27 +0200

    Initial commit

---
+++ src/First.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Vladimir on 05.11.2016.
+ */
+public class First {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("BYE!!!");
+
+
+    }
+}
